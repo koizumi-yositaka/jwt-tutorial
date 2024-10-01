2adefa7adc095426bb277a1cc6cd249b2c6f2700
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2024-10-01 21:42:39 +0900

    push test

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app= express()
+app.use(express.json())
+const PORT = 5002
+
+app.get("/",(req,res)=>{
+    res.send({aa:"sdfs"})
+})
+
+app.listen(PORT,()=>{
+    console.log("server run")
+})
