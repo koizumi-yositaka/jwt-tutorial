27efff16360e11ebad4e424966ca075ec12e9301
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2024-10-02 23:53:43 +0900

    config

--- app/server.js
+++ app/server.js
@@ -1,10 +1,15 @@
 const express = require("express")
+const authRouter = require("./routes/auth-router")
+
+
 const app= express()
 app.use(express.json())
+app.use("/auth",authRouter)
+
 const PORT = 5002
 
 app.get("/",(req,res)=>{
-    res.send({aa:"XXXXX"})
+    res.send({aa:process.env.REFRESH_TOKEN_DURATION_MINUTE})
 })
 
 app.listen(PORT,()=>{
