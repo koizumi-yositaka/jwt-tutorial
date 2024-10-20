edc4bbd1d4c46ec2bdd27c73ae71f6c831dbaa50
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2024-10-21 04:04:09 +0900

    test test

--- app/test/app.test.js
+++ app/test/app.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../app");
 
-describe("Express App Tests",() => {
+describe("Express App Tests22",() => {
   it("should return REACT_PORT from /api", async () => {
     const response = await request(app).get("/api");
     expect(response.statusCode).toBe(200);
